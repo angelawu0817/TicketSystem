6de6ab69972e8d6d7c2a01957fac92097786173d
angelawu0817 <angelawu0817@gmail.com>
2021-06-15 22:59:14 +0800

    test

--- TicketSystem/Program.cs
+++ TicketSystem/Program.cs
@@ -17,6 +17,7 @@ namespace TicketSystem
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+
         }
     }
 }
